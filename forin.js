24186e0acf15e0030100100e3ae301ea901dd0ad
srinadhreddy1729 <126847497+srinadhreddy1729@users.noreply.github.com>
2023-05-24 12:50:54 +0530

    Update forin.js

--- forin.js
+++ forin.js
@@ -1,1 +1,11 @@
 
+function Data()
+{
+    let oo=new Array(56,67,89,12,34);
+    for(var kk of oo)
+    {
+        console.log(kk)
+    }
+}
+Data(); //it allows you to access the emumerable properties of an object
+         //we can use strings ,arrays and collections like set,map
